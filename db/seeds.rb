50a945752d4b62b9974ab74ec165c8318bff426e
Gonzague Gru <gonzague@agricool.co>
2018-08-17 10:57:21 +0200

    mister cocktail back quasi fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+require 'rubygems'
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+file = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+
+data_hash = JSON.parse(file)
+data_hash["drinks"].each do |data|
+  Ingredient.create(name: data["strIngredient1"])
+end
+
